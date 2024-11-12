3c864d1f700c12e42bf3a4473933a8083a16eca4
ThomasVu <samduykhanh01@gmail.com>
2024-11-11 20:21:40 -0500

    Editted education

--- src/lib/data/education.ts
+++ src/lib/data/education.ts
@@ -3,16 +3,16 @@ import type { Education } from '../types';
 
 export const items: Array<Education> = [
 	{
-		degree: 'Bachelor degree of Computer Science',
+		degree: 'Honour Specialization in Computer Science',
 		description: '',
-		location: 'Tunisia',
+		location: 'Canada',
 		logo: Assets.Unknown,
 		name: '',
 		organization: 'ISTIC',
-		period: { from: new Date(2020, 0, 1), to: new Date(2022, 5, 1) },
+		period: { from: new Date(2022, 9, 1), to: new Date(2026, 5, 1) },
 		shortDescription: '',
 		slug: 'dummy-education-item',
-		subjects: ['C', 'Algorithm', 'Algebra', 'Python', 'C++', 'Java', 'English']
+		subjects: ["C", "C++", "Python", "Java", "DSA", "Networks", "OS", "AI", "Data Science"]
 	},
 ];
 
