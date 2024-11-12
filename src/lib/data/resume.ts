188eb28ae3d5733a78383e60ee69aaaf137cdb93
ThomasVu <samduykhanh01@gmail.com>
2024-11-12 12:09:34 -0500

    Testing the routes

--- src/lib/data/resume.ts
+++ src/lib/data/resume.ts
@@ -1,3 +1,7 @@
-export const data = '';
+import { base } from '$app/paths';
+
+export const data = `${base}/documents/resume.pdf`; // Dynamic path with base
+
+// export const data = '/documents/resume.pdf';
 
 export const title = 'Resumé';
